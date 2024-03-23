3c1b2e21246f87cafbccbe75f147932b3f0a7b85
StealthySturgeon <hasar.ali.1997+github@gmail.com>
2024-03-23 00:43:37 -0400

    Add nvim config

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Uncomment line and use map to map keys
+-- local map = vim.keymap.set
