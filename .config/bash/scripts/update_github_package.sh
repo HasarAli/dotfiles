eeba1c8ebe345f100c8eb3307070791908a61f3f
StealthySturgeon <hasar.ali.1997+github@gmail.com>
2024-12-07 23:17:00 -0500

    Handle more cases

--- .config/bash/scripts/update_github_package.sh
+++ .config/bash/scripts/update_github_package.sh
@@ -6,9 +6,17 @@ SCRIPTS="$HOME/.config/bash/scripts"
 get_installed_version() {
     local command_name="$1"
 
-    if ! command -v "$command_name" >/dev/null; then
+    if ! command -v "$command_name" >/dev/null 2>&1; then
         printf "none"
     else
+        local version
+        version=$("$command_name" --version 2>/dev/null | awk 'NR==1{print $2}')
+        if [[ -z "$version" ]]; then
+            printf "unknown"
+        else
+            printf "%s" "$version"
+        fi
+    fi
 }
 
 validate_asset_data() {
@@ -50,9 +58,25 @@ main() {
     local installed_version
     installed_version=$(get_installed_version "$command_name")
 
-    if [[ "$installed_version" != "$latest_version" ]]; then
+    if [[ "$installed_version" == "none" ]]; then
+        printf "Missing: '%s' is not installed. Installing version '%s'...\n" "$command_name" "$latest_version"
+        if ! "$SCRIPTS/fetch_and_install_package.sh" "$command_name" "$url"; then
+            printf "Error: Failed to install '%s'.\n" "$command_name" >&2
+            exit 1
+        fi
+        printf "Installation of '%s' completed.\n" "$command_name"
+        exit 0
+    elif [[ "$installed_version" == "unknown" ]]; then
+        printf "Unknown: Installed version of '%s' could not be determined. Latest version '%s'.\n" "$command_name" "$latest_version" >&2
+        exit 1
+    elif [[ "$installed_version" != "$latest_version" ]]; then
         printf "Update required for '%s': Installed version '%s', latest version '%s'.\n" "$command_name" "$installed_version" "$latest_version"
-        "$SCRIPTS/fetch_and_install_package.sh" "$command_name" "$url" || exit 1
+        if ! "$SCRIPTS/fetch_and_install_package.sh" "$command_name" "$url"; then
+            printf "Error: Failed to update '%s'.\n" "$command_name" >&2
+            exit 1
+        fi
+        printf "Update completed for '%s'.\n" "$command_name"
+        exit 0
     fi
 }
 
